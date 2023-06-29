1ed9de2558d6107f2c9c7de16f2840fd81852937
Brett Wallin <brettwallin1999@gmail.com>
2023-06-28 21:32:19 -0500

    added timeline

---
+++ my-app/src/Components/TimelineElements.js
@@ -0,0 +1,29 @@
+let TimelineElements = [
+    {
+      id: 1,
+      title: "Heartland Community College",
+      location: "Normal, Illinois",
+      description: "AS Computer Science",
+      date: "August 2021 - May 2023",
+      icon: "school",
+    },
+    {
+      id: 2,
+      title: "Software Engineer Intern at State Farm",
+      location: "Bloomington, Illinois",
+      description: "Test",
+      date: "May 2023 - August 2023",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "Illinois State University",
+      location: "Normal, Illinois",
+      description: "BS Computer Science",
+      date: "August 2023 - May 2025",
+      icon: "school",
+    },
+  ];
+  
+  export default TimelineElements;
+  
--- my-app/src/Pages/AboutMe.js
+++ my-app/src/Pages/AboutMe.js
@@ -1,10 +1,57 @@
 import React from "react";
 import '../Components/About.css'
+import { ReactComponent as WorkIcon } from "../Images/work.svg";
+import { ReactComponent as SchoolIcon } from "../Images/school.svg";
+
+import TimelineElements from "../Components/TimelineElements";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
 
 export default function About() {
-    return(
-        <div className="about-me">
-            <h1>Test</h1>
+  let workIconStyles = { background: "cyan" };
+  let schoolIconStyles = { background: "cyan" };
+
+  return (
+    <div className="about-me">
+        <div>
+            <h1 className="title">My Experience</h1>
+            <VerticalTimeline className="custom-timeline">
+                {TimelineElements.map((element) => {
+                let isWorkIcon = element.icon === "work";
+
+                return (
+                    <VerticalTimelineElement
+                        key={element.key}
+                        date={element.date}
+                        dateClassName="date"
+                        iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                        icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+                        >
+                        <h3 className="vertical-timeline-element-title">
+                            {element.title}
+                        </h3>
+                        <h5 className="vertical-timeline-element-subtitle">
+                            {element.location}
+                        </h5>
+                        <p id="description">{element.description}</p>
+                    </VerticalTimelineElement>
+                );
+            })}
+            </VerticalTimeline>
         </div>
-    )
+
+        <div>
+            <h1 className="skills-title">My Skills</h1>
+            <div className="skills">
+                
+            </div>
+        </div>
+    </div>
+  );
 }
+
